fd6ad235b7b058c4b660d21d2cd1fc585c3a9f8b
Rahul Sen <rahul.sen@mindfiresolutions.com>
2023-02-24 14:24:35 +0530

    Added map funciton with hard coded value

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      name: { firstName: "Rahul", lastName: "Sen" },
-
-      company: "Mindfire",
+      monsters: [
+        {
+          id: 1,
+          name: "linda",
+        },
+        {
+          id: 2,
+          name: "Frank",
+        },
+        {
+          id: 3,
+          name: "Jacky",
+        },
+      ],
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            My name is {this.state.name.firstName} {this.state.name.lastName}{" "}
-            and I am working in {this.state.company}!!
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  // its a updater function
-                  return { name: { firstName: "Riyan", lastName: "Sen" } };
-                },
-                () => {
-                  // its the callback after the update function is done
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        {/* using map to iterate the array  */}
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
