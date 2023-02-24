3abea601efdf1d9a03fff094cf83848000f9c69b
Rahul Sen <rahul.sen@mindfiresolutions.com>
2023-02-24 17:46:44 +0530

    small optimization

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -21,29 +22,37 @@ class App extends React.Component {
         })
       );
   }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
         <input
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={(event) => {
-            console.log(event.target.value);
-            const searchString = event.target.value.toLocaleLowerCase();
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchString);
-            });
-            this.setState(() => {
-              return { monsters: filteredMonsters };
-            });
-          }}
+          onChange={onSearchChange}
         />
         {/* using map to iterate the array  */}
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
+            <div
+              style={{ border: "1px solid black", marginTop: "5px" }}
+              key={monster.id}
+            >
+              <h2>{monster.name}</h2>
             </div>
           );
         })}
