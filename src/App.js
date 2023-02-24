da00061719314db04d7849dc6bf7de9955ee5a37
Rahul Sen <rahul.sen@mindfiresolutions.com>
2023-02-24 12:45:19 +0530

    basic example of using state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: { firstName: "Rahul", lastName: "Sen" },
+
+      company: "Mindfire",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            My name is {this.state.name.firstName} {this.state.name.lastName}{" "}
+            and I am working in {this.state.company}!!
+          </p>
+          <button
+            onClick={() => {
+              this.setState(
+                () => {
+                  // its a updater function
+                  return { name: { firstName: "Riyan", lastName: "Sen" } };
+                },
+                () => {
+                  // its the callback after the update function is done
+                  console.log(this.state);
+                }
+              );
+            }}
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
