963734c8e4d832e26480444b5a022879a18611bd
Rahul Sen <rahul.sen@mindfiresolutions.com>
2023-02-24 16:16:44 +0530

    Change state based on the search input

--- src/App.js
+++ src/App.js
@@ -5,26 +5,40 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          id: 1,
-          name: "linda",
-        },
-        {
-          id: 2,
-          name: "Frank",
-        },
-        {
-          id: 3,
-          name: "Jacky",
-        },
-      ],
+      monsters: [],
     };
   }
 
+  // life cycle method
+  // this will call once , when component will be mounted to the dom for the first time.
+  // we will add our api fetching code here
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
   render() {
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
         {/* using map to iterate the array  */}
         {this.state.monsters.map((monster) => {
           return (
